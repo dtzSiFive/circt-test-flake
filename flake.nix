d08ce14ab9800c7fd40f2938a131a97235df6101
Will Dietz <will.dietz@sifive.com>
2022-11-02 12:05:03 -0500

    flake: continue to manually populate arguments for now.

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
          circt = circt-nix.packages.${system};
          results = import ./test.nix {
            inherit circt firrtl-src;
-           inherit (pkgs) lib time runCommand;
+           inherit (pkgs) lib diffoscope time runCommand;
          };
          in {
            packages = results // {
