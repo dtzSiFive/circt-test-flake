17218d31d3d0e91cec6bda958e7f61b54d9531a1
Will Dietz <will.dietz@sifive.com>
2022-11-02 12:04:43 -0500

    refactor

--- flake.nix
+++ flake.nix
@@ -24,44 +24,15 @@
       (system: let
          pkgs = nixpkgs.legacyPackages.${system};
          circt = circt-nix.packages.${system};
+         results = import ./test.nix {
+           inherit circt firrtl-src;
+           inherit (pkgs) lib time runCommand;
+         };
          in {
-         packages = rec {
-           results = import ./test.nix {
-             inherit circt firrtl-src;
-             inherit (pkgs) lib time runCommand;
+           packages = results // {
+            default = results.diff;
            };
-          default = results.circt-pp; }; 
-       });
-
-#        let pkgs = nixpkgs.legacyPackages.${system};
-#            newLLVMPkgs = pkgs.callPackage ./llvm.nix {
-#              inherit llvm-submodule-src;
-#              llvmPackages = pkgs.llvmPackages_git;
-#            };
-#        in rec {
-#          devShells = {
-#            default = import ./shell.nix { inherit pkgs; };
-#            git = import ./shell.nix {
-#               inherit pkgs;
-#               llvmPkgs = pkgs.llvmPackages_git; # NOT same as submodule.
-#            };
-#          };
-#          packages = flake-utils.lib.flattenTree (newLLVMPkgs // rec {
-#            default = circt; # default for `nix build` etc.
-#
-#            circt = pkgs.callPackage ./circt.nix {
-#              inherit circt-src;
-#              inherit (newLLVMPkgs) libllvm mlir llvmUtilsSrc;
-#            };
-#            circt-pp = circt.override { circt-src = circt-pp-src; };
-#            slang = pkgs.callPackage ./slang.nix {
-#              inherit slang-src;
-#            };
-#          });
-#          apps = pkgs.lib.genAttrs [ "firtool" "circt-lsp-server" ]
-#            (name: flake-utils.lib.mkApp { drv = packages.circt; inherit name; });
-#        }
-#      );
+         });
 
   nixConfig = {
     extra-substituters = [ "https://dtz-circt.cachix.org" ];
