876067b06283979379cd1cf8d62e65c44ccc2eb7
Will Dietz <will.dietz@sifive.com>
2022-11-02 13:05:44 -0500

    test.nix: markdown is nice, but gh-pages dies on what's produced. drop.

--- test.nix
+++ test.nix
@@ -31,7 +31,6 @@ let
         mkdir -p "$o_d"
         diffoscope --no-default-limits "$a_d/$d.sv" "$b_d/$d.sv" \
           --max-page-diff-block-lines 100000 \
-          --markdown "$o_d/$d.md" \
           --html "$o_d/$d.html" \
           --text "$o_d/$d.diff" \
           || (echo "Files are different, see output files for details")
